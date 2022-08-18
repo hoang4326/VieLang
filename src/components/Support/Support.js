3623812c1f067f903829f64dae7d5a8d0256b553
hoang4326 <84314103+hoang4326@users.noreply.github.com>
2022-08-18 23:38:04 +0700

    support page

--- src/components/Support/Support.js
+++ src/components/Support/Support.js
@@ -5,16 +5,16 @@ export default function Support(){
     return (
         <section className='contact'>
             <div className='content'>
-                <h2>Contact us</h2>
-                <p>Hello</p>
+                <h2>Contact Us</h2>
+                <p>Please get in touch and our expert support team will answer all your questions.</p>
             </div>
-            <div className='container'>
+            <div className='containerSupport'>
                 <div className='contactInfo'>
                     <div className='box'>
                         <div className='icon'><i class="fa fa-map-marker" aria-hidden="true"></i></div>
                         <div className='text'>
                             <h3>Address</h3>
-                            <p>Golden Park Tower - 2 Phạm Văn Bạch</p>
+                            <p>Golden Park Tower - 2 Pham Van Bach - Cau Giay - Ha Noi</p>
                         </div>
                     </div>
                     <div className='box'>
@@ -31,6 +31,26 @@ export default function Support(){
                             <p>nguyenvantai0717@gmail.com</p>
                         </div>
                     </div>
+                </div>
+                <div className="contactForm">
+                    <form>
+                        <h2>Send Message</h2>
+                        <div className="inputBox">
+                            <input type='text' name='' required='required'/>
+                            <span>Full name</span>
+                        </div>
+                        <div className="inputBox">
+                            <input type='text' name='' required='required'/>
+                            <span>Email</span>
+                        </div>
+                        <div className="inputBox">
+                            <textarea required='required'></textarea>
+                            <span>Type your message...</span>
+                        </div>
+                        <div className="inputBox">
+                            <input type='submit' name='' value='Send'/>
+                        </div>
+                    </form>
                 </div>
             </div>
         </section>
