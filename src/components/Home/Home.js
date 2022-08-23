fd3732410f4645e65b1d1ffcf46febe6107c5d3b
hoang4326 <84314103+hoang4326@users.noreply.github.com>
2022-08-23 21:41:39 +0700

    update desktop

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -26,7 +26,7 @@ export default function Home(){
                 </div>
                 
                 <div className='description-item'>
-                    <img src={require('../../assets/image/advantage2.png')} className='description-item-image'/>
+                    <img src={require('../../assets/image/explanation.png')} className='description-item-image'/>
                     <h3 className='description-item-title'>Detailed explanations</h3>
                 <p className='description-item-text'>VieLang understands the unique complexity of your chosen language. Here, you will learn with clarity and context. No more guessing around.</p>
                 </div>
@@ -38,19 +38,37 @@ export default function Home(){
                 </div>
 
                 <div className='description-item'>
-                    <img src={require('../../assets/image/advantage4.png')} className='description-item-image'/>
+                    <img src={require('../../assets/image/audio.png')} className='description-item-image'/>
                     <h3 className='description-item-title'>HD audio</h3>
                 <p className='description-item-text'>Learn with lessons voiced by native speakers from the beginning. Don’t let robotic TTS audio compromise your listening and speaking skills.</p>
                 </div>
 
                 <div className='description-item'>
-                    <img src={require('../../assets/image/advantage5.png')} className='description-item-image'/>
+                    <img src={require('../../assets/image/story.png')} className='description-item-image'/>
                     <h3 className='description-item-title'>Engaging stories</h3>
                 <p className='description-item-text'>Practice speaking with hundreds of engaging stories which introduce interesting cultural facts while teaching new words and expressions.</p>
                 </div>
                 
             </div>
         </section>
+        {/* <section className='aboutSection'>
+            <div className='containerSection'>
+                <div className='rowSection'>
+                    <div className='col-12 col-lg-6 align-content-lg-stretch'>
+                        <header className='headerSection'>
+                            <h2 className='entry-tile'>About VieLang</h2>
+                            <p className ='pSection'>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium.</p>
+                        </header>
+                    </div>
+                    <div className='ezuca-video position-relative'>
+                        <div className='col-12 col-lg-6 flex align-content-center mt-5 mt-lg-0'>
+                            <img classname ='imgSection' src='https://preview.colorlib.com/theme/ezuca/images/xvideo-screenshot.png.pagespeed.ic.1KmQ_Dh4Lv.webp' />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section> */}
     </div>
     )
 }
