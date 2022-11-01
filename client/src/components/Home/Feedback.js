628f1d9c793ef473b6c72ce937c6f7a7ee5be36c
hoang4326 <84314103+hoang4326@users.noreply.github.com>
2022-11-01 16:30:30 +0700

    update content homepage

--- client/src/components/Home/Feedback.js
+++ client/src/components/Home/Feedback.js
@@ -2,25 +2,25 @@ export const dataFeedback = [
     {
         id: 1,
         name: 'Ethel Davis',
-        content: '" A+ app, can learn multiple languages for a reasonable price, much less than what language centers charge "',
-        img:'https://preview.colorlib.com/theme/educature/img/faculty/xf1.jpg.pagespeed.ic.XbWix9NC"A"A.webp'
+        content: '" A+ app, can learn Vietnamese for free, much less than what language centers charge "',
+        img:'https://upload.motgame.vn/photos/motgame-vn/2022/05/avatar-doi-cho-2-nguoi-51.jpg'
     },
     {
         id: 2,
-        name: 'Ethel Davis',
-        content: '" A+ app, can learn multiple languages for a reasonable price, much less than what language centers charge "',
-        img:'https://preview.colorlib.com/theme/educature/img/faculty/xf1.jpg.pagespeed.ic.XbWix9NC"A"A.webp'
+        name: 'Dadvid James',
+        content: '" Despite not being as well-known as some other language learning applications, VieLang is really more effective and affordable than most of them.  "',
+        img:'https://haycafe.vn/wp-content/uploads/2022/02/Anh-avatar-that-tinh.jpg'
     },
     {
         id: 3,
-        name: 'Ethel Davis',
-        content: '" A+ app, can learn multiple languages for a reasonable price, much less than what language centers charge "',
-        img:'https://preview.colorlib.com/theme/educature/img/faculty/xf1.jpg.pagespeed.ic.XbWix9NC"A"A.webp'
+        name: 'Seiley Kyle',
+        content: '" It has a great design and everything works well with only a few minor issues. "',
+        img:'https://vcdn1-dulich.vnecdn.net/2020/11/21/1-1605972569.jpg?w=1200&h=0&q=100&dpr=1&fit=crop&s=Gs5VBQ9eOvNSi6tNWUhkRQ'
     },
     {
         id: 4,
-        name: 'Ethel Davis',
-        content: '" A+ app, can learn multiple languages for a reasonable price, much less than what language centers charge "',
+        name: 'Thomas Micheal',
+        content: '" The lessons and vocabulary are organized really effectively. It does so in a style that is incredibly memorable while also teaching vocabulary and grammar. "',
         img:'https://preview.colorlib.com/theme/educature/img/faculty/xf1.jpg.pagespeed.ic.XbWix9NC"A"A.webp'
     }
 ]
